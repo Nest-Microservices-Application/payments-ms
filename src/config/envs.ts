f5d1043dc946e410bc23b3d16e4e82b2ce16b44b
Martín Cruz <martincruz1426@gmail.com>
2024-07-24 17:10:06 -0300

    Adding missing envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,13 +3,21 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+
   STRIPE_SECRET_KEY: string;
+  STRIPE_ENDPOINT_SECRET: string;
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+
     STRIPE_SECRET_KEY: joi.string().required(),
+    STRIPE_ENDPOINT_SECRET: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -21,5 +29,9 @@ const envVars: EnvVars = value;
 
 export const envs = {
   PORT: envVars.PORT,
+  
   STRIPE_SECRET_KEY: envVars.STRIPE_SECRET_KEY,
+  STRIPE_ENDPOINT_SECRET: envVars.STRIPE_ENDPOINT_SECRET,
+  STRIPE_SUCCESS_URL: envVars.STRIPE_SUCCESS_URL,
+  STRIPE_CANCEL_URL: envVars.STRIPE_CANCEL_URL,
 };
