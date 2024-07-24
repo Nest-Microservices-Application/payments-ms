1a2e88bda31dc4d1d7e6408334a2ce36376f4b66
Martín Cruz <martincruz1426@gmail.com>
2024-07-24 17:12:20 -0300

    Stripe payments implemented

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,75 @@
 import { Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
+import Stripe from 'stripe';
+import { envs } from '../config';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.STRIPE_SECRET_KEY);
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const line_items = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await this.stripe.checkout.sessions.create({
+      //TODO connect order id
+      payment_intent_data: {
+        metadata: {
+          orderId: orderId,
+        },
+      },
+      line_items: line_items,
+      mode: 'payment',
+      success_url: envs.STRIPE_SUCCESS_URL,
+      cancel_url: envs.STRIPE_CANCEL_URL,
+    });
+
+    return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        envs.STRIPE_ENDPOINT_SECRET,
+      );
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        //TODO use the microservice and process the data
+        console.log({
+          metadata: chargeSucceeded.metadata,
+          orderId: chargeSucceeded.metadata.orderId,
+        });
+        break;
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return res.status(200).json({});
+  }
+}
