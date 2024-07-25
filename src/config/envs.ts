1f3ccdf913b6edbd2428893eca2b7105e974bbf8
Martín Cruz <martincruz1426@gmail.com>
2024-07-25 02:32:15 -0300

    Envs updated an nats service added to connect payments with nats

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -8,6 +8,8 @@ interface EnvVars {
   STRIPE_ENDPOINT_SECRET: string;
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
@@ -18,10 +20,15 @@ const envsSchema = joi
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS.split(','),
+});
 
 if (error) throw new Error(`Config validation error: ${error.message}`);
 
@@ -29,9 +36,11 @@ const envVars: EnvVars = value;
 
 export const envs = {
   PORT: envVars.PORT,
-  
+
   STRIPE_SECRET_KEY: envVars.STRIPE_SECRET_KEY,
   STRIPE_ENDPOINT_SECRET: envVars.STRIPE_ENDPOINT_SECRET,
   STRIPE_SUCCESS_URL: envVars.STRIPE_SUCCESS_URL,
   STRIPE_CANCEL_URL: envVars.STRIPE_CANCEL_URL,
+
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
